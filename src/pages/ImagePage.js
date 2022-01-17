487ece7dda54e69adeda63b42e92311d2b9326d1
Nadia <nadiakrakowski@gmail.com>
2022-01-16 21:36:06 -0600

    remove leftover dependency

--- src/pages/ImagePage.js
+++ src/pages/ImagePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 import { fetchImage, imageSelector } from '../slices/image'
 
