520af35ed82afbb15390eeec9db4b274c0079e5e
Nadia Konieczny <nadiakrakowski@gmail.com>
2022-01-19 04:39:06 -0600

    prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from "react";
-import { render } from "react-dom";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider } from "react-redux";
+import React from 'react'
+import { render } from 'react-dom'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
-import App from "./App";
-import rootReducer from "./slices";
+import App from './App'
+import rootReducer from './slices'
 
-import "./index.css";
+import './index.css'
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({ reducer: rootReducer })
 
 render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
