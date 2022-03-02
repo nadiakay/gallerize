c3339eda3d7451d12b1f04db4311fff1475c588e
Nadia Konieczny <nadiakrakowski@gmail.com>
2022-03-01 19:09:42 -0600

    using HashRouter for gh-pages compatibility

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import { Layout } from './routes/Layout'
 import { Gallery } from './routes/Gallery'
@@ -8,14 +8,14 @@ import { Image } from './routes/Image'
 const App = () => {
   return (
     <div className="app">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Gallery />} />
             <Route path="/:source/:id" element={<Image />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   )
 }
